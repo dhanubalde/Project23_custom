55ae2f7aff58111ac27decbb7021f48958e6eebc
dhan <kedanubalde41@gmail.com>
2023-09-06 19:34:10 +0800

    updating new repo

--- apps/urls.py
+++ apps/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.user_list_view)
+    path('',views.user_list_view),
+    path('<int:pk>/', views.user_detail_view),
+    path('<int:pk>/update/', views.user_update_view),
+    path('<int:pk>/delete/', views.user_delete_view)
 ]
--- apps/views.py
+++ apps/views.py
@@ -9,3 +9,29 @@ class UserListCreateAPIView(generics.ListCreateAPIView):
     lookup_field = 'pk'
     
 user_list_view = UserListCreateAPIView.as_view()
+
+
+class UserDetailAPIView(generics.RetrieveAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerialiser
+    lookup_field = 'pk'
+    
+user_detail_view = UserDetailAPIView.as_view()
+
+class UserUpdateAPIView(generics.RetrieveUpdateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerialiser
+    lookup_field = 'pk'
+    
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    
+user_update_view = UserUpdateAPIView.as_view()
+
+class UserDeleteAPIView(generics.RetrieveDestroyAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerialiser
+    lookup_field = 'pk'
+    
+
+user_delete_view = UserDeleteAPIView.as_view()
